99243c05900c568a1955342afb5956ab18a1dd0d
Roberto Palacios <rpalacios@csumb.edu>
2018-01-27 02:17:18 -0800

    activity one

---
+++ Activity1a/activity1a/Assets/MyFirstBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstBehavior : MonoBehaviour {
+
+    public int repeatNo = 10;
+    public int frame = 0;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello World");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (frame == 50)
+        {
+            if (repeatNo > 0)
+            {
+                Debug.Log(repeatNo);
+                repeatNo--;
+
+            }
+            frame = 0;
+        }
+        else if (repeatNo != 0)
+            frame++;
+    }
+}
